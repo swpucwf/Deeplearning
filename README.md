af8a5a814f338d941fe591e66ba4487951e58e2a
chenweifeng <15882085601@.com>
2021-01-21 23:51:24 +0800

    加个说明

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Deeplearning
 陈伟峰的学习之路
+- 开始学习啦
+- torch,opencv,torchvision版本
+    - 1.7.1+cu110
+    - 4.5.1
+    - 0.8.2+cu110
---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+import cv2
+import torchvision
+
+print(torch.__version__)
+print(cv2.__version__)
+print(torchvision.__version__)
